03d630a0f82d2357af6d88a87a12100c94d2434c
German Martinez <german.mtz.solis@gmail.com>
2017-07-25 11:40:26 -0500

    feat: modified html structure

---
+++ js/style.js
@@ -0,0 +1,13 @@
+( ()=> {
+	'use strict';
+
+	const tabs = [
+		// Example of pushed object
+		// {
+		// 	tabName:'Tab Uno',
+		// 	tabContentTitle: 'titulo tab uno',
+		// 	tabContentParagraph: 'lorem 1'
+		// },
+	];
+
+})();
